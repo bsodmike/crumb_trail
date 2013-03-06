fb881a9ef0206aeea6b24cb5f73e10e72f5a73c3
Michael de Silva <michael@mwdesilva.com>
2013-03-07 00:33:48 +0530

    Generate new Rails 3.2.12 plugin with spec/dummy and without test/

---
+++ lib/crumb_trail/version.rb
@@ -0,0 +1,3 @@
+module CrumbTrail
+  VERSION = "0.0.1"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '2ea99d4bad1a8b26e11fc8216668a6bb97edb9a312898af41ba73ee0e8ac2d12950cf20a5218c58f150eea74559dd326f5dd3df96590ee279354c5dc451a52c8'

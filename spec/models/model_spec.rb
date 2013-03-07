b81289830a47e08a45ddac3f21afcee42cb30df9
Michael de Silva <michael@mwdesilva.com>
2013-03-07 13:14:25 +0530

    Directly setup test objects with AR::Base.connection.execute
    This allows us to ignore mass-assignment whitelist configuration of
    the target application.

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "ActiveRecord Models that declare `has_crumb_trail`" do
   before(:all) do
-    @book = Book.create :title => 'Book'
+    ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => "../dummy/db/test.sqlite3"
+    ActiveRecord::Base.connection.execute "INSERT INTO books (title, created_at, updated_at) VALUES ('Book', '#{Time.now}', '#{Time.now}')"
+    @book = Book.first
   end
 
   it "should respond to Book#icanhazcheeseburger?" do
